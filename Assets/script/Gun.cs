f37026116437b4bd7e6a3ce6ee1c2a6de23226cb
jungkko <kim35990960@gmail.com>
2023-10-20 16:25:43 +0900

    총 완성

--- Assets/script/Gun.cs
+++ Assets/script/Gun.cs
@@ -5,31 +5,51 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public int Damage = 10;
+    public AudioClip gun_fire;
+    public AudioClip empty_magazine;
+    public AudioClip Reroad;
 
     RaycastHit hit;
     float max_ray = 15f;
     AudioSource audio;
 
+
     private void Start()
     {
-        audio = gameObject.GetComponent<AudioSource>();
+        audio =gameObject.GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)&&GameManager.Game_Mg.burnt_index!=0) 
+        if (Input.GetKeyDown(KeyCode.E)) 
         {
-            Debug.DrawRay(transform.position, transform.forward * max_ray, Color.blue, 0.1f);
-            if (Physics.Raycast(transform.position, transform.forward, out hit, max_ray))
+            StartCoroutine(GameManager.Game_Mg.burnt_use());
+            if (GameManager.Game_Mg.burnt_index != 0)
             {
-                StartCoroutine(GameManager.Game_Mg.burnt_use());
-                audio.Play();
-                if (hit.transform.CompareTag("monster"))
+                Debug.DrawRay(transform.position, transform.forward * max_ray, Color.blue, 0.1f);
+                if (Physics.Raycast(transform.position, transform.forward, out hit, max_ray))
                 {
-                    StartCoroutine(hit.transform.GetComponent<moster_st>().monster_damage(Damage));
-                }
+                    audio.clip = gun_fire;
+                    audio.Play();
+                    if (hit.transform.CompareTag("monster"))
+                    {
+                        StartCoroutine(hit.transform.GetComponent<moster_st>().monster_damage(Damage));
+                    }
 
+                }
             }
+            else
+            {
+                audio.clip = empty_magazine;
+                audio.Play();
+            }
+
+        }
+        if(Input.GetKeyDown(KeyCode.R)&&GameManager.Game_Mg.burnt_index != 5)
+        {
+            audio.clip = Reroad;
+            audio.Play();
+            GameManager.Game_Mg.burnt_index = 5;
         }
     }
 }

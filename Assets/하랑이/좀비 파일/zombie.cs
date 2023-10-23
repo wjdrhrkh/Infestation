6616936008b1418e73c50818a9606524baadf1aa
조하랑 <wawa111027@gmail.com>
2023-10-23 14:01:04 +0900

    좀비 생성 수정
    좀비 생성 y좌표

--- Assets/하랑이/좀비 파일/zombie.cs
+++ Assets/하랑이/좀비 파일/zombie.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class zombie : MonoBehaviour
 {
     public GameObject objectToSpawn; // 소환할 오브젝트의 프리팹을 설정하기 위한 변수
+    public float top = 0;
 
     public float spawnInterval = 2.0f; // 소환 간격
     public float spawnRadius = 5.0f; // 소환할 위치의 반경
@@ -19,7 +20,7 @@ public class zombie : MonoBehaviour
             // 랜덤한 위치 계산
             Vector3 randomPosition = transform.position + Random.insideUnitSphere * spawnRadius;
             // Y 좌표를 조정하여 지면 위에 소환되도록 함
-            randomPosition.y = 0;
+            randomPosition.y = top;
 
             // 오브젝트 소환
             Instantiate(objectToSpawn, randomPosition, Quaternion.identity);

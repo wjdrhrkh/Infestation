6b520ab7449c13ca1d7351a4d9a61f889be3d7ee
jungkko <kim35990960@gmail.com>
2023-10-25 19:54:09 +0900

    오큘러스 샘플 설정, 총 코드 수정 주석 수

--- Assets/script/Gun.cs
+++ Assets/script/Gun.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using Oculus;
 
 public class Gun : MonoBehaviour
 {
@@ -14,15 +15,17 @@ public class Gun : MonoBehaviour
     float max_ray = 15f;
     AudioSource audio;
 
+    private OVRInput.Controller controller;
 
     private void Start()
     {
-        audio =gameObject.GetComponent<AudioSource>();//오디오 소스 불러오기
+        audio = gameObject.GetComponent<AudioSource>();//오디오 소스 불러오기
+        controller= OVRInput.Controller.RTouch;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))//e키가 눌리고
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, controller))//오른손 검지 트리거 키를 누르면
         {
             if (GameManager.Game_Mg.burnt_index > 0)//총알 개수가 0개 이하가 아니면
             {
@@ -45,7 +48,7 @@ public class Gun : MonoBehaviour
             }
 
         }
-        if(Input.GetKeyDown(KeyCode.R) && GameManager.Game_Mg.burnt_index != 5)//총알이 5개가 아닌 상태에서 r키를 누르면
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, controller) && GameManager.Game_Mg.burnt_index != 5)//총알이 5개가 아닌 상태에서 오른손 트리거 키를 누르면
         {
             audio.clip = Reroad;//클립에 재장전소리 대입
             audio.Play();//오디오 재생
